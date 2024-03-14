6412dc111bfe1e0a8712903cbfd3823e40139565
muhammedalaa1 <muhammedalaa2213@gmail.com>
2024-03-14 23:58:48 +0200

    added taositfy

--- src/pages/SignUpStudent/index.jsx
+++ src/pages/SignUpStudent/index.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from '@/api/axios';
 import { Navbar } from '@/layout';
+import { toast } from 'react-toastify';
 
 const SignUpStudent = () => {
   const navigate = useNavigate();
